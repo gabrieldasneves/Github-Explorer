ae7864f755060127761903285aaefe273b5ab3c6
Gabriel das Neves <galvesdasneves@gmail.com>
2022-08-30 23:15:52 -0300

    First component + trying to fix React18 render bug

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,13 @@
-import {render} from "react-dom";
-import { App } from './App';
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import {App} from './App';
 
- render(<App />,document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
