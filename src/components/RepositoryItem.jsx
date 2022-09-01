1ba1d8d21ddb759d9aa49d547fe0ef928dee40d6
Gabriel das Neves <galvesdasneves@gmail.com>
2022-08-31 23:10:20 -0300

    props with objects

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props){
     return(
         <li>
-            <strong>{props.repository}</strong>
-            <p>{props.description}</p>
-            <a href={props.link}>
+            <strong>{props.repository?.name ?? 'Default'}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.link}>
                 Acessar repositório
             </a>
         </li>
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,21 @@
 import {RepositoryItem} from "./RepositoryItem";
 
+const repository = {
+    name:"Unform1" ,
+    description:"Forms in React with props" ,
+    link:"https://github.com/gabrieldasneves?tab=repositories"
+}
+
+
 export function RepositoryList(){
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem 
-                    repository="Unform1" 
-                    description="Forms in React with props" 
-                    link="https://github.com/gabrieldasneves?tab=repositories"/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     );
