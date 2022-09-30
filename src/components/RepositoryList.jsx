52925009ce5169e9399c5c049f273b5d4a857778
Gabriel das Neves <galvesdasneves@gmail.com>
2022-09-30 17:45:37 -0300

    fetching github API and using hooks useState and UseEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,9 @@
+import { useState, useEffect } from "react";
 import {RepositoryItem} from "./RepositoryItem";
-import '../styles/repositories.scss'
+import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
+
 const repository = {
     name:"Unform1" ,
     description:"Forms in React with props" ,
@@ -8,6 +12,15 @@ const repository = {
 
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    }, [repositories]);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
